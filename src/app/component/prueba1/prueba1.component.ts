a5279a934c180718638b84c9640c1f6b9b336330
Luis Quintal <lquintal.comasw@gmail.com>
2021-09-11 13:52:48 -0500

    Se sube app

---
+++ src/app/component/prueba1/prueba1.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prueba1',
+  templateUrl: './prueba1.component.html',
+  styleUrls: ['./prueba1.component.css']
+})
+export class Prueba1Component implements OnInit {
+
+  producto = 'acer'
+  precio = 10000
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
